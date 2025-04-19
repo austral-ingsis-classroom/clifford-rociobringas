5248da96b0b60ac8bf4ad4e53b39a0d54c7ecda5
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-04-19 11:05:03 -0300

    mkdir

---
+++ src/main/java/edu/austral/ingsis/clifford/MkdirCommand.java
@@ -0,0 +1,15 @@
+package edu.austral.ingsis.clifford;
+
+public class MkdirCommand implements Command {
+  private final String directoryName;
+
+  public MkdirCommand(String directoryName){
+    this.directoryName = directoryName;
+  }
+
+  @Override
+  public String execute(FileSystem fileSystem) {
+    fileSystem.createDirectory(directoryName);
+    return directoryName + " directory created";
+  }
+}
