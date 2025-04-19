fc9c3a74b3bb9264cebaa82c040180d950f65a85
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-04-19 10:01:49 -0300

    file system element, clase para cualquier cosa ue sea dentro del cli.

--- src/main/java/edu/austral/ingsis/clifford/FileSystem.java
+++
@@ -1,4 +0,0 @@
-package edu.austral.ingsis.clifford;
-
-public interface FileSystem {}
-// no tengo readme, probar si puedo comitear
---
+++ src/main/java/edu/austral/ingsis/clifford/FileSystemElement.java
@@ -0,0 +1,45 @@
+package edu.austral.ingsis.clifford;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// representa cualquier cosa que este dentro del file system
+public abstract class FileSystemElement {
+  String name;
+  Directory parent;
+
+  public FileSystemElement(String name, Directory parent){
+    this.name = name;
+    this.parent = parent;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public String getPath() {
+    if (parent == null) {
+      return "/"; // Estoy en el root
+    }
+
+    List<String> path = new ArrayList<>();
+    FileSystemElement current = this;
+
+    // voy fijandome los parents
+    while (current.parent != null) {
+      path.add(current.name);
+      current = current.parent;
+    }
+
+    // invierto porque agrego al reves
+    Collections.reverse(path);
+
+    // agrego "/"
+    return "/" + String.join("/", path);
+  }
+
+  public boolean isDirectory(){};
+
+
+}
